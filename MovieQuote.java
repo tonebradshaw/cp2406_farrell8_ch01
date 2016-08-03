4d037d4611bc41964dda0b6f8561cd1b4e364287
tonebradshaw <anthony.bradshaw@my.jcu.edu.au>
2016-08-03 12:07:57 +1000

    Prac1Extra

---
+++ MovieQuote.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Tony on 3/08/2016.
+ */
+public class MovieQuote {
+
+    public static void main(String[] args) {
+
+        System.out.println("I'll be back");
+
+    }
+}
---
+++ TableAndChairs.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Tony on 3/08/2016.
+ */
+public class TableAndChairs {
+
+    public static void main(String[] args) {
+
+        System.out.println("X                      X");
+        System.out.println("X                      X");
+        System.out.println("X      XXXXXXXXXX      X");
+        System.out.println("XXXXX  X        X  XXXXX");
+        System.out.println("X   X  X        X  X   X");
+        System.out.println("X   X  X        X  X   X");
+    }
+}
